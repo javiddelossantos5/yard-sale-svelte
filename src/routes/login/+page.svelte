41d8be9798f3fe0eebf749f91512a5a508b7f426
Javid De Los Santos <118299733+javidsantos@users.noreply.github.com>
2025-10-20 16:47:58 -0600

    I can now sign up users

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -28,6 +28,60 @@
 	let error = $state<string | null>(null);
 	let success = $state<string | null>(null);
 
+	// US States for dropdown
+	const states = [
+		{ code: 'AL', name: 'Alabama' },
+		{ code: 'AK', name: 'Alaska' },
+		{ code: 'AZ', name: 'Arizona' },
+		{ code: 'AR', name: 'Arkansas' },
+		{ code: 'CA', name: 'California' },
+		{ code: 'CO', name: 'Colorado' },
+		{ code: 'CT', name: 'Connecticut' },
+		{ code: 'DE', name: 'Delaware' },
+		{ code: 'FL', name: 'Florida' },
+		{ code: 'GA', name: 'Georgia' },
+		{ code: 'HI', name: 'Hawaii' },
+		{ code: 'ID', name: 'Idaho' },
+		{ code: 'IL', name: 'Illinois' },
+		{ code: 'IN', name: 'Indiana' },
+		{ code: 'IA', name: 'Iowa' },
+		{ code: 'KS', name: 'Kansas' },
+		{ code: 'KY', name: 'Kentucky' },
+		{ code: 'LA', name: 'Louisiana' },
+		{ code: 'ME', name: 'Maine' },
+		{ code: 'MD', name: 'Maryland' },
+		{ code: 'MA', name: 'Massachusetts' },
+		{ code: 'MI', name: 'Michigan' },
+		{ code: 'MN', name: 'Minnesota' },
+		{ code: 'MS', name: 'Mississippi' },
+		{ code: 'MO', name: 'Missouri' },
+		{ code: 'MT', name: 'Montana' },
+		{ code: 'NE', name: 'Nebraska' },
+		{ code: 'NV', name: 'Nevada' },
+		{ code: 'NH', name: 'New Hampshire' },
+		{ code: 'NJ', name: 'New Jersey' },
+		{ code: 'NM', name: 'New Mexico' },
+		{ code: 'NY', name: 'New York' },
+		{ code: 'NC', name: 'North Carolina' },
+		{ code: 'ND', name: 'North Dakota' },
+		{ code: 'OH', name: 'Ohio' },
+		{ code: 'OK', name: 'Oklahoma' },
+		{ code: 'OR', name: 'Oregon' },
+		{ code: 'PA', name: 'Pennsylvania' },
+		{ code: 'RI', name: 'Rhode Island' },
+		{ code: 'SC', name: 'South Carolina' },
+		{ code: 'SD', name: 'South Dakota' },
+		{ code: 'TN', name: 'Tennessee' },
+		{ code: 'TX', name: 'Texas' },
+		{ code: 'UT', name: 'Utah' },
+		{ code: 'VT', name: 'Vermont' },
+		{ code: 'VA', name: 'Virginia' },
+		{ code: 'WA', name: 'Washington' },
+		{ code: 'WV', name: 'West Virginia' },
+		{ code: 'WI', name: 'Wisconsin' },
+		{ code: 'WY', name: 'Wyoming' }
+	];
+
 	async function handleLogin(event: Event) {
 		event.preventDefault();
 
@@ -438,16 +492,19 @@
 								State *
 							</label>
 							<div class="mt-1">
-								<input
+								<select
 									id="reg-state"
 									name="state"
-									type="text"
 									required
 									bind:value={registerData.location.state}
-									class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 focus:border-blue-500 focus:ring-blue-500 focus:outline-none sm:text-sm dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-400 dark:focus:ring-blue-400"
-									placeholder="State"
+									class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 focus:border-blue-500 focus:ring-blue-500 focus:outline-none sm:text-sm dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:focus:border-blue-400 dark:focus:ring-blue-400"
 									disabled={loading}
-								/>
+								>
+									<option value="">Select State</option>
+									{#each states as state}
+										<option value={state.code}>{state.name}</option>
+									{/each}
+								</select>
 							</div>
 						</div>
 						<div>
@@ -609,7 +666,6 @@
 					</div>
 				</div>
 			{/if}
-
 		</div>
 	</div>
 </div>
